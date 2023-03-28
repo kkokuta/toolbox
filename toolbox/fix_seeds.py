a0ce6814ad6fb8a3dd4c708e0475632147437410
kkokuta <kokuta.kazuma.r3@dc.tohoku.ac.jp>
2023-03-28 17:09:47 +0900

    PYTHONHASHSEED が数値通りに設定されているかどうか assertion

--- toolbox/fix_seeds.py
+++ toolbox/fix_seeds.py
@@ -1,10 +1,13 @@
+import os
 import random
 import numpy as np
 import torch
 
 
 def fix_seeds(seed=42):
+    assert os.environ["PYTHONHASHSEED"] == str(seed), f"set PYTHONHASHSEED (={seed})"
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
