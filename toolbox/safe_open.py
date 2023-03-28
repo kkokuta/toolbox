8671ea3d8c6c17a9d6dade0d7c0f1eb5144fa1a9
kkokuta <kokuta.kazuma.r3@dc.tohoku.ac.jp>
2023-03-28 14:41:42 +0900

    ディレクトリがない場合に作成する open 関数の拡張関数を実装

---
+++ toolbox/safe_open.py
@@ -0,0 +1,12 @@
+import os
+
+def safe_open(file_path, *args, **kwargs):
+    # ファイルが含まれるディレクトリを取得
+    dir_path = os.path.dirname(file_path)
+
+    # ディレクトリが存在しない場合は作成
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # ファイルをオープンして返す
+    return open(file_path, *args, **kwargs)
